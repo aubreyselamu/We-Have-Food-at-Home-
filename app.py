02334cdf4d571ee4d29040cdab665d47dcfc9211
Aubrey Selamu-Bell <aubrey.selamu@Aubreys-MacBook-Pro.local>
2022-02-17 19:40:12 -0500

    fixing heroku application

--- app.py
+++ app.py
@@ -16,13 +16,13 @@ CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///fridge')
-# # uri = os.getenv("DATABASE_URL") 
-# # if uri.startswith("postgres://"):
-# #     uri = uri.replace("postgres://", "postgresql://", 1)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///fridge')
+uri = os.getenv("DATABASE_URL") 
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
 
-# app.config["DATABASE_URI"] = uri
+app.config["DATABASE_URI"] = uri
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
